e0aa8be49cdbd0a01988c1971e693181671482df
Prescott Murphy <prurph@gmail.com>
2024-08-19 12:43:58 -0400

    Complete Exercise 022

--- src/015-essential-types-and-annotations/022-all-types.problem.ts
+++ src/015-essential-types-and-annotations/022-all-types.problem.ts
@@ -1,5 +1,11 @@
 let example1: string = "Hello World!";
-let example2: string = 42;
-let example3: string = true;
-let example4: string = Symbol();
-let example5: string = 123n;
+let example2: number = 42;
+let example3: boolean = true;
+let example4: Symbol = Symbol();
+let example5: bigint = 123n;
+
+let example1_inferred = "Hello World!";
+let example2_inferred = 42;
+let example3_inferred = true;
+let example4_inferred = Symbol();
+let example5_inferred = 123n;
