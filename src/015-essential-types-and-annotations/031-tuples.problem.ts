3b785cd7d89df6ccf1f7b38c061e2a36025bc07d
Prescott Murphy <prurph@gmail.com>
2024-08-19 17:33:37 -0400

    Complete Exercise 031

--- src/015-essential-types-and-annotations/031-tuples.problem.ts
+++ src/015-essential-types-and-annotations/031-tuples.problem.ts
@@ -1,15 +1,16 @@
 import { Expect, Equal } from "@total-typescript/helpers";
 
-const setRange = (range: Array<number>) => {
+// Naming the parameters in tuple doesn't bind them in the funciton body;
+// it's purely for signature type hints.
+const setRange = (range: [x: number, y: number]) => {
   const x = range[0];
   const y = range[1];
-
   // Do something with x and y in here
 
   // x and y should both be numbers!
   type tests = [
     Expect<Equal<typeof x, number>>,
-    Expect<Equal<typeof y, number>>,
+    Expect<Equal<typeof y, number>>
   ];
 };
 
