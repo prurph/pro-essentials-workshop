4a4ffa292fbe65c8badb396b9d7a322ec15082d5
Prescott Murphy <prurph@gmail.com>
2024-08-20 11:09:59 -0400

    Complete Exercise 044

--- src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
+++ src/016.5-ide-superpowers/044-manually-triggering-autocomplete.problem.ts
@@ -8,12 +8,15 @@ const acceptsObj = (obj: MyObj) => {};
 
 acceptsObj({
   // Autocomplete in here!
+  foo: "foo",
+  bar: 1,
+  baz: true,
 });
 
 document.addEventListener(
   // Autocomplete this string!
-  "",
+  "DOMContentLoaded",
   (event) => {
     console.log(event);
-  },
+  }
 );
