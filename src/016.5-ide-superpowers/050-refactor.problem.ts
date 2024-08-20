491cfbcbfd00d23ec31c43782e40559c2b078162
Prescott Murphy <prurph@gmail.com>
2024-08-20 16:35:32 -0400

    Complete Exercise 050

--- src/016.5-ide-superpowers/050-refactor.problem.ts
+++ src/016.5-ide-superpowers/050-refactor.problem.ts
@@ -1,5 +1,9 @@
 const func = () => {
   // Refactor this to be its own function
-  const randomPercentage = `${(Math.random() * 100).toFixed(2)}%`;
+  const randomPercentage = getRandomPercentage();
   console.log(randomPercentage);
 };
+
+function getRandomPercentage() {
+  return `${(Math.random() * 100).toFixed(2)}%`;
+}
