6f2e8a5f1f3237254a9abc7a9d4822bd0d2482da
Prescott Murphy <prurph@gmail.com>
2024-08-21 13:54:17 -0400

    Complete Exercise 085

--- src/020-objects/085-index-signatures-with-defined-keys.problem.ts
+++ src/020-objects/085-index-signatures-with-defined-keys.problem.ts
@@ -1,4 +1,12 @@
-interface Scores {}
+// This requires the defined keys to exist, and the index signature
+// allows any other string: number mapping as well. Also note that
+// the added items must match the index signature.
+interface Scores {
+  math: number;
+  english: number;
+  science: number;
+  [subject: string]: number;
+}
 
 // @ts-expect-error science is missing!
 const scores: Scores = {
