83fb2e87aede25db51be038304f867eaec64d74b
Prescott Murphy <prurph@gmail.com>
2024-08-21 08:46:17 -0400

    Complete Exercise 068

--- src/018-unions-and-narrowing/068-returning-never-to-narrow.problem.ts
+++ src/018-unions-and-narrowing/068-returning-never-to-narrow.problem.ts
@@ -1,10 +1,13 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 
-const throwError = (message: string): undefined => {
+const throwError = (message: string) => {
   throw new Error(message);
 };
 
 const handleSearchParams = (params: { id?: string }) => {
+  // As the bottom of the type hierarchy, never is assignable to anything, so by making `throwError`
+  // return never, id is typed as `string`, and the test passes. Rather than explicitly marking the
+  // return type never, removing the annotation allows it to be inferred as such.
   const id = params.id || throwError("No id provided");
 
   type test = Expect<Equal<typeof id, string>>;
