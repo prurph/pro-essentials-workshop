091337a02ae74b6155a68cd65a23d410f6221521
Prescott Murphy <prurph@gmail.com>
2024-08-20 22:10:04 -0400

    Complete Exercise 066

--- src/018-unions-and-narrowing/066-narrowing-unknown-to-a-value.problem.ts
+++ src/018-unions-and-narrowing/066-narrowing-unknown-to-a-value.problem.ts
@@ -1,8 +1,22 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 import { expect, it } from "vitest";
 
-const parseValue = (value: unknown) => {
-  if (true) {
+const parseValue = (value: unknown): string => {
+  // Optional chaining is runtime, so const id = value?.data?.id doesn't work here
+  // because it doesn't narrow the type at all for the compiler. Must check x != null
+  // because typeof null === "object" in javascript. It's "the intentional absence of
+  // an object" the same way NaN is "the intentional absence of a number" and hence
+  // typeof NaN === "number".
+  // zod is a cool library for doing easier validation of stuff like this.
+  if (
+    typeof value === "object" &&
+    value !== null &&
+    "data" in value &&
+    typeof value.data === "object" &&
+    value.data !== null &&
+    "id" in value.data &&
+    typeof value.data.id === "string"
+  ) {
     return value.data.id;
   }
 
