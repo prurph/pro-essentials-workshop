ac18e89cabb272482df5f4da68c5b4c92219f568
Prescott Murphy <prurph@gmail.com>
2024-08-20 20:55:12 -0400

    Complete Exercise 062

--- src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
+++ src/018-unions-and-narrowing/062-throwing-errors-to-narrow.problem.ts
@@ -1,7 +1,13 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 
-const appElement = document.getElementById("app");
+const appElement = document.getElementById("app")!;
 
 // How do I ensure that appElement is defined?
+
+// Another option instead of nullish-coalescing operator (!).
+// Compiler knows on line 13 appElement can't be undefined.
+// if (!appElement) {
+//   throw new Error("Could not find #app element");
+// }
 
 type Test = Expect<Equal<typeof appElement, HTMLElement>>;
