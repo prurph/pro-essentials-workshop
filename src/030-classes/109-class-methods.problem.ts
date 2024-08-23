2d936bb743e981ce7ce88b140c03f423a0edd9b0
Prescott Murphy <prurph@gmail.com>
2024-08-23 09:14:07 -0400

    Complete Exercise 109

--- src/030-classes/109-class-methods.problem.ts
+++ src/030-classes/109-class-methods.problem.ts
@@ -3,6 +3,16 @@ import { expect, it } from "vitest";
 class CanvasNode {
   x = 0;
   y = 0;
+
+  // Generally don't need to chose between this syntax and function
+  // syntax in classes, however this way `this` is always bound to
+  // the instance, whereas function move(x, y) ... isn't, and stuff like
+  // const fn = canvasNode.move
+  // fn(10, 20) // => error; `this` is undefined
+  move = (x: number, y: number) => {
+    this.x = x;
+    this.y = y;
+  };
 }
 
 it("Should be able to move", () => {
