d3514c6d7f386b885be72b71ffc70773d1e8bb16
Prescott Murphy <prurph@gmail.com>
2024-08-25 13:10:32 -0400

    Complete Exercise 152

--- src/050-the-weird-parts/152-excess-properties-warnings.problem.ts
+++ src/050-the-weird-parts/152-excess-properties-warnings.problem.ts
@@ -1,3 +1,7 @@
+// Ways to enable excess property warnings (e.g. adding `search` to `options` below)
+// 1. Annotate variable as desired type
+// 2. Use `satisfies` with desired type on variable
+// 3. Pass parameter directly inline to a method that takes the type
 interface FetchOptions {
   url: string;
   method?: string;
@@ -5,7 +9,8 @@ interface FetchOptions {
   body?: string;
 }
 
-const options = {
+// 1: add `FetchOptions` annotation
+const options: FetchOptions = {
   url: "/",
   method: "GET",
   headers: {
@@ -15,8 +20,10 @@ const options = {
   search: new URLSearchParams({
     limit: "10",
   }),
-};
+}; // 2: `satisfies FetchOptions` here (instead of annotation above)
 
 const myFetch = async (options: FetchOptions) => {};
 
+// 3: call as `myFetch({url: ...})` rather than assigning to variable,
+// causing TS to type it as a `FetchOptions` based on the function type
 myFetch(options);
