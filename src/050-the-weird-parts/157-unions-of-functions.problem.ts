31946c5dd35aaedcbb49a2111f8d0ccbaf340bf4
Prescott Murphy <prurph@gmail.com>
2024-08-25 18:00:37 -0400

    Complete Exercise 157

--- src/050-the-weird-parts/157-unions-of-functions.problem.ts
+++ src/050-the-weird-parts/157-unions-of-functions.problem.ts
@@ -5,8 +5,22 @@ const objOfFunctions = {
 };
 
 const format = (input: string | number | boolean) => {
+  // One way to do this:
+  // switch (typeof input) {
+  //   case "string":
+  //     return objOfFunctions.string(input);
+  //   case "number":
+  //     return objOfFunctions.number(input);
+  //   case "boolean":
+  //     return objOfFunctions.boolean(input);
+  // }
+
+  // Fancy way:
   const inputType = typeof input as "string" | "number" | "boolean";
   const formatter = objOfFunctions[inputType];
 
-  return formatter(input);
+  // Argument type here is the intersection of the various formatter
+  // argument types, which means it's never. You can annotate the
+  // argument as such and it will just work (at runtime).
+  return formatter(input as never);
 };
