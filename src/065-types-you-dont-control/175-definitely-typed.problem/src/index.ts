2bb70aea590acc6f046131cfd602e36b88d73ce5
Prescott Murphy <prurph@gmail.com>
2024-08-26 11:06:29 -0400

    Complete Exercise 175

--- src/065-types-you-dont-control/175-definitely-typed.problem/src/index.ts
+++ src/065-types-you-dont-control/175-definitely-typed.problem/src/index.ts
@@ -1,5 +1,9 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 
+// Exists because of `pnpm i @types/diff@5.2.0 -D`
+// I had issues with "no default export" when I didn't add the exact version
+// to align with the version of Diff being used. Without it; pnpm installed
+// @types/diff@5.2.1, but there doesn't appear to _be_ a diff@5.2.1
 import Diff from "diff";
 
 const message = "Hello, world!";
