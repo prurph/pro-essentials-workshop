944e305a0af8be14126bbc7afe69201db0e8c706
Prescott Murphy <prurph@gmail.com>
2024-08-26 11:28:32 -0400

    Complete Exercise 175.5

--- src/065-types-you-dont-control/175.5-modifying-process-env.problem/src/index.ts
+++ src/065-types-you-dont-control/175.5-modifying-process-env.problem/src/index.ts
@@ -1,5 +1,8 @@
 import { Equal, Expect } from "@total-typescript/helpers";
 
+// process.env is NodeJS.ProcessEnv, a ProcessEnv interface inside the NodeJS
+// namespace. This means we can declaration merge by declaring `interface ProcessEnv`
+// inside a `declare namespace NodeJS` and adding our env vars to the interface.
 const envVariable = process.env.MY_ENV_VAR;
 
 type test = Expect<Equal<typeof envVariable, string>>;
